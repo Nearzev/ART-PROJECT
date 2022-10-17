4e5740d1930f18aff0e89a15bf71726eab40ced5
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2022-10-17 16:08:03 +0300

    add sliders on page
    1) vertical slider
    2) horizontal slider

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,21 @@
 import {modals} from './modules/modals';
+import {sliders} from './modules/sleders';
 
 window.addEventListener('DOMContentLoaded', () => {
     'usestrict'
+
     modals();
+    sliders({
+        slidesSelector:'.feedback-slider-item', 
+        pos: '', 
+        prev:'.main-prev-btn', 
+        next:'.main-next-btn'
+    });
+    sliders({
+        slidesSelector:'.main-slider-item', 
+        pos: 'vertical', 
+        prev:'', 
+        next:'',
+    });
 });
 
